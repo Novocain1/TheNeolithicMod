008ee367a1e66d0dc1eac3a6c67e77cb1d86a735
Novocain1 <22160223+Novocain1@users.noreply.github.com>
2018-11-09 12:49:37 -0500

    Update ItemSwappable, again example in sisal-rope.sjon

--- Neolithic/Content/Item/ItemSwappable.cs
+++ Neolithic/Content/Item/ItemSwappable.cs
@@ -13,28 +13,37 @@ namespace TheNeolithicMod
 {
     class ItemSwappable : Item
     {
-
+        Dictionary<AssetLocation, AssetLocation> swapMapping = new Dictionary<AssetLocation, AssetLocation>();
         public override void OnLoaded(ICoreAPI api)
         {
             base.OnLoaded(api);
+            AssetLocation[][] arraySwapMapping = Attributes["swapBlocks"].AsObject<AssetLocation[][]>();
+            foreach (var val in arraySwapMapping)
+            {
+                swapMapping[val[0]] = val[1];
+            }
         }
         public override void OnHeldInteractStart(IItemSlot slot, IEntityAgent byEntity, BlockSelection blockSel, EntitySelection entitySel, ref EnumHandHandling handling)
         {
             base.OnHeldInteractStart(slot, byEntity, blockSel, entitySel, ref handling);
             int swapRate = slot.Itemstack.Collectible.Attributes["swapRate"].AsInt(0);
+            AssetLocation toCode;
             Block block = byEntity.World.BlockAccessor.GetBlock(blockSel.Position);
             BlockPos pos = blockSel.Position;
-            Block fromBlock = api.World.GetBlock(new AssetLocation(slot.Itemstack.Collectible.Attributes["fromCode"].AsString()));
-            Block toBlock = api.World.GetBlock(new AssetLocation(slot.Itemstack.Collectible.Attributes["toCode"].AsString()));
 
-            if (block != null && block == fromBlock && slot.Itemstack.StackSize >= swapRate) 
+            if (!swapMapping.TryGetValue(block.Code, out toCode))
+            {
+                return;
+            }
+
+            if (slot.Itemstack.StackSize >= swapRate)
             {
                 if (swapRate != 0)
                 {
                     slot.TakeOut(swapRate);
                 }
                 api.World.PlaySoundAt(block.Sounds.Place, pos.X, pos.Y, pos.Z);
-                api.World.BlockAccessor.SetBlock(toBlock.BlockId, pos);
+                api.World.BlockAccessor.SetBlock(api.World.GetBlock(toCode).BlockId, pos);
             }
         }
     }
