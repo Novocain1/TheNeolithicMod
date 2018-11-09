9afdc4a06b19286d509f19fa639a1dcd476a86b5
Novocain1 <22160223+Novocain1@users.noreply.github.com>
2018-11-09 13:22:38 -0500

    Fix Crash When Clicking On Air

--- Neolithic/Content/Item/ItemSwappable.cs
+++ Neolithic/Content/Item/ItemSwappable.cs
@@ -26,24 +26,26 @@ namespace TheNeolithicMod
         public override void OnHeldInteractStart(IItemSlot slot, IEntityAgent byEntity, BlockSelection blockSel, EntitySelection entitySel, ref EnumHandHandling handling)
         {
             base.OnHeldInteractStart(slot, byEntity, blockSel, entitySel, ref handling);
-            int swapRate = slot.Itemstack.Collectible.Attributes["swapRate"].AsInt(0);
-            AssetLocation toCode;
-            Block block = byEntity.World.BlockAccessor.GetBlock(blockSel.Position);
-            BlockPos pos = blockSel.Position;
-
-            if (!swapMapping.TryGetValue(block.Code, out toCode))
+            if (blockSel != null)
             {
-                return;
-            }
+                int swapRate = slot.Itemstack.Collectible.Attributes["swapRate"].AsInt(0);
+                Block block = byEntity.World.BlockAccessor.GetBlock(blockSel.Position);
+                BlockPos pos = blockSel.Position;
 
-            if (slot.Itemstack.StackSize >= swapRate)
-            {
-                if (swapRate != 0)
+                if (!swapMapping.TryGetValue(block.Code, out AssetLocation toCode))
                 {
-                    slot.TakeOut(swapRate);
+                    return;
                 }
-                api.World.PlaySoundAt(block.Sounds.Place, pos.X, pos.Y, pos.Z);
-                api.World.BlockAccessor.SetBlock(api.World.GetBlock(toCode).BlockId, pos);
+
+                if (slot.Itemstack.StackSize >= swapRate)
+                {
+                    if (swapRate != 0)
+                    {
+                        slot.TakeOut(swapRate);
+                    }
+                    api.World.PlaySoundAt(block.Sounds.Place, pos.X, pos.Y, pos.Z);
+                    api.World.BlockAccessor.SetBlock(api.World.GetBlock(toCode).BlockId, pos);
+                }
             }
         }
     }
