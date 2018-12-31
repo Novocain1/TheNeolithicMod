7354b419a05793b89000beee23e7ba94ac482b69
Novocain1 <22160223+Novocain1@users.noreply.github.com>
2018-12-31 09:09:07 -0500

    Include Carrycapacity Future

--- Neolithic/TheNeolithicMod.cs
+++ Neolithic/TheNeolithicMod.cs
@@ -16,7 +16,7 @@ namespace TheNeolithicMod
             api.RegisterBlockBehaviorClass("LampPostBehavior", typeof(LampPostBehavior));
             api.RegisterBlockBehaviorClass("RotateNinety", typeof(RotateNinety));
             api.RegisterBlockBehaviorClass("ChimneyBehavior", typeof(ChimneyBehavior));
-            api.RegisterBlockBehaviorClass("CostTest", typeof(CostTest));
+            //api.RegisterBlockBehaviorClass("CostTest", typeof(CostTest));
 
             //Block
             api.RegisterBlockClass("BlockPlaceOnDrop", typeof(BlockPlaceOnDrop));
